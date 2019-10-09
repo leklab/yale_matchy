21a773fe6ab0c41cb32cf8bdc321f1d17438dac6
monkollek <monkol.lek@yale.edu>
2019-10-09 04:48:02 +0000

    genotype scoring implemented

--- matchy_app.py
+++ matchy_app.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 from flask import Flask, render_template, request, redirect, jsonify
 app = Flask(__name__)
 
@@ -46,7 +48,7 @@ def get_gene_constraint(transcript_id):
     }     
   }""" % (transcript_id)
 
-  print(query)
+  #print(query)
 
   res = requests.post('http://gnomad.broadinstitute.org/api', json={'query': query})
 
@@ -58,6 +60,57 @@ def get_gene_constraint(transcript_id):
     return({'data': 'error'}) 
 
 
+def get_gnomad_freq(variant_id):
+    query = """
+    {
+        variant(dataset: gnomad_r2_1, variantId:"%s"){
+        variantId
+        ref
+        alt
+        ... on GnomadVariantDetails{
+          exome{
+            ac
+            an
+            filters
+          }
+          genome{
+            ac
+            an
+            filters
+          }
+        }  
+      }
+    }""" % (variant_id)
+
+    #print(query)
+
+    res = requests.post('http://gnomad.broadinstitute.org/api', json={'query': query})
+
+    if res.ok:      
+        res_json = res.json()
+
+        if not 'errors' in res_json:
+            variant_details = res_json['data']['variant']
+
+            ac_total = 0
+            an_total = 0
+
+            if 'exome' in variant_details:
+                ac_total += variant_details['exome']['ac']
+                an_total += variant_details['exome']['an']
+
+            if 'genome' in variant_details:
+                ac_total += variant_details['genome']['ac']
+                an_total += variant_details['genome']['an']
+
+
+
+            #print res.json()
+            #print("AC: %d AN: %d AF: %.20e" % (ac_total, an_total, (1.0*ac_total/an_total)))
+            return ac_total/an_total
+
+        else:
+            return 0.0
 
 
 
@@ -155,7 +208,23 @@ def search_matchy(request_json):
     test_json = request_json['genomicFeatures'][0]
     #print(test_json['gene']['id'])
 
-    results = gene_only_match(test_json['gene']['id'])
+    variant_str = ''
+
+    if 'variant' in test_json:
+        chrom = test_json['variant']['referenceName'] if 'referenceName' in test_json['variant'] else ""
+        
+        #start and end coordinates are whacky but +1 works for snps for now    
+        chrom_start = str(test_json['variant']['start']+1) if 'start' in test_json['variant'] else ""
+        
+        chrom_ref = test_json['variant']['referenceBases'] if 'referenceBases' in test_json['variant'] else ""
+        chrom_alt = test_json['variant']['alternateBases'] if 'alternateBases' in test_json['variant'] else ""
+
+        variant_str = "%s-%s-%s-%s" % (chrom,chrom_start,chrom_ref,chrom_alt)
+        print(variant_str)
+
+
+
+    results = genotype_match(test_json['gene']['id'],variant_str)
     print(results)
 
     return results
@@ -163,7 +232,7 @@ def search_matchy(request_json):
   	#print(res)
 
 
-def gene_only_match(gene_name):
+def genotype_match(gene_name, variant):
 
     #geneENSG = geneDic[gene_name]
 
@@ -183,7 +252,7 @@ def gene_only_match(gene_name):
     #At the moment genotype and phenotype are equal weighting (i.e. 0.5 each)
     #Gene only matches are scored based on pLI
     #TO DO: If inheritance model is specified use something different for recessive
-    matchy_score = round(0.5*gene_constraint_pLI,3)
+    gene_score = gene_constraint_pLI
 
     results = []
 
@@ -198,6 +267,41 @@ def gene_only_match(gene_name):
 
     for hit in res['hits']['hits']:
         doc = hit['_source']['doc']
+        #print(doc)
+
+        variant_scores = [0.0]
+
+        for g in doc['genomicFeatures']:
+            print(g['gene']['id'])
+            #{u'assembly': u'GRCh37', u'start': 42929130, u'alternateBases': u'A', u'referenceName': u'17', u'end': 42929131, u'referenceBases': u'G'}
+            if g['gene']['id'] == ensembl_gene_id and g['variant']:
+                #print(g['variant'])
+                chrom = g['variant']['referenceName'] if 'referenceName' in g['variant'] else ""
+                
+                #start and end coordinates are whacky but +1 works for snps for now    
+                chrom_start = str(g['variant']['start']+1) if 'start' in g['variant'] else ""
+                
+                chrom_ref = g['variant']['referenceBases'] if 'referenceBases' in g['variant'] else ""
+                chrom_alt = g['variant']['alternateBases'] if 'alternateBases' in g['variant'] else ""
+
+                variant_str = "%s-%s-%s-%s" % (chrom,chrom_start,chrom_ref,chrom_alt)
+                print(variant_str)
+
+                if variant_str == variant:
+                    gnomad_af = get_gnomad_freq(variant_str)
+                    print("Variant matches: %s gnomAD freq: %.3e" % (variant_str,gnomad_af))
+                    variant_scores.append(1-gnomad_af)                    
+
+
+
+
+        max_variant_score = max(variant_scores)
+        print("gene_score: %f variant_score: %f" % (gene_score,max_variant_score))
+
+        if max_variant_score > 0:
+            matchy_score = round(0.5*(max_variant_score),3)
+        else:
+            matchy_score = round(0.5*(gene_score),3)
 
         results.append({"score": {"patient": matchy_score}, "patient": {"id": doc['id'], "contact": doc['contact']}})
 
